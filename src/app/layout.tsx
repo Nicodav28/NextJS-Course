1126d6d5ba9f28d6e49c23141f2db898c0c8fabd
Nicolas David Estevez <nicolas.estevez@infodeclat.com>
2024-04-21 01:38:38 -0500

    🔧 chore(server.ts): cambiar la variable port de minúsculas a mayúsculas PORT
    🔧 chore(server.ts): añadir soporte para la variable de entorno process.env.PORT
    
    🔧 chore(globals.css): eliminar archivo globals.css
    
    🔧 chore(layout.tsx): actualizar import de Header y añadir import de Footer
    
    🔧 chore(page.module.css): eliminar archivo page.module.css
    
    🔧 chore(page.tsx): actualizar import de Description y Hero, añadir import de MainProducts
    
    🔧 chore(Description.tsx): añadir componente Description
    
    🔧 chore(Hero.tsx): añadir componente Hero
    
    🔧 chore(MainProducts.tsx): añadir componente MainProducts
    
    🔧 chore(Footer.tsx): añadir componente Footer
    
    🔧 chore(Header.tsx): añadir componente Header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
-import { Header } from "app/components/Header";
+import { Header } from "app/components/shared/Header";
+import { Footer } from "app/components/shared/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -17,8 +17,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header></Header>
+        <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/home/Description/Description.tsx
@@ -0,0 +1,16 @@
+export const Description = () => {
+  return (
+    <section>
+      <h1>Description</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium,
+        autem, consequuntur corporis delectus doloremque dolorum eos expedita
+        facere fugit harum impedit ipsam itaque, iusto laudantium maiores maxime
+        minima nobis nihil nulla numquam obcaecati officia optio pariatur quasi
+        quidem quo quos quos repellendus repudiandae sapiente sequi sint soluta
+        sunt tempora tenetur tempore tenetur unde ut veritatis voluptates
+        voluptatum.
+      </p>
+    </section>
+  );
+};
