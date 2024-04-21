adaf1538c9ce740abe077a1d03ff9fa8fa6da410
Nicolas David Estevez <nicolas.estevez@infodeclat.com>
2024-04-21 17:31:32 -0500

    🔧 fix(server.ts): cambiar la variable port de minúsculas a mayúsculas PORT
    🔧 fix(next.config.mjs): añadir nueva configuración para Sass
    
    🔧 fix(package.json): añadir dependencias de Sass
    
    📸 feat(public/images): añadir imagen de descripción
    
    🔧 fix(src/app/layout.tsx): importar estilos globales de Sass
    
    📝 feat(src/components/home/Description): añadir componente de descripción de la página de inicio
    
    📝 feat(src/components/home/Hero): añadir componente de héroe de la página de inicio
    
    🔧 fix(src/sass): añadir archivos de variables y estilos globales de Sass

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,16 +1,17 @@
+import styles from "./Description.module.sass";
+
 export const Description = () => {
   return (
-    <section>
-      <h1>Description</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium,
-        autem, consequuntur corporis delectus doloremque dolorum eos expedita
-        facere fugit harum impedit ipsam itaque, iusto laudantium maiores maxime
-        minima nobis nihil nulla numquam obcaecati officia optio pariatur quasi
-        quidem quo quos quos repellendus repudiandae sapiente sequi sint soluta
-        sunt tempora tenetur tempore tenetur unde ut veritatis voluptates
-        voluptatum.
-      </p>
+    <section className={styles.Description}>
+      <img src="/images/description.jpeg" alt="products marketplace" />
+      <div className={styles.Description__text}>
+        <h2>Bring the future today</h2>
+        <p>
+          Future World: Your Gateway to Tomorrow&apos;s Tech! Dive into a world
+          of cutting-edge gadgets and gear. Stay ahead of the curve and redefine
+          your digital lifestyle with us.
+        </p>
+      </div>
     </section>
   );
 };
--- src/components/home/Hero/Hero.tsx
+++ src/components/home/Hero/Hero.tsx
@@ -1,3 +1,11 @@
+import styles from "./Hero.module.sass";
+
 export const Hero = () => {
-  return <h1>Hero</h1>;
+  const { Hero } = styles;
+  return (
+    <section className={Hero}>
+      <h1>Future World</h1>
+      <h2>Empowering Your Tomorrow, Today!</h2>
+    </section>
+  );
 };
